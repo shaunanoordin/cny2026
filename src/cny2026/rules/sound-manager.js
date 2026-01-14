e68504d6e191943d932ad7ff15ac8360b8ce34c4
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-01-14 18:00:33 +0000

    SoundManager: add toggleMute()

--- src/cny2026/rules/sound-manager.js
+++ src/cny2026/rules/sound-manager.js
@@ -11,7 +11,7 @@ export default class SoundManager extends Rule {
   constructor (app) {
     super(app)
     this._type = 'sound-manager'
-    this.muted = true
+    this.muted = false
 
     this.pickUpSound = new Howl({
       src: ['assets/sound-pick-up.wav']
@@ -24,9 +24,16 @@ export default class SoundManager extends Rule {
     this.destinationReachedSound = new Howl({
       src: ['assets/sound-destination-reached.wav']
     })
+
+    this.toggleMute = this.toggleMute.bind(this)
+    document.getElementById('button-sound').addEventListener('click', this.toggleMute)
+    document.getElementById('button-sound').dataset.muted = (this.muted) ? 'true' : 'false'
   }
 
-  deconstructor () {}
+  deconstructor () {
+
+
+  }
 
   playPickUp () {
     if (this.muted) return
@@ -42,5 +49,10 @@ export default class SoundManager extends Rule {
     if (this.muted) return
     // this.dropOffSound.stop()
     this.destinationReachedSound.play()
+  }
+
+  toggleMute () {
+    this.muted = !this.muted
+    document.getElementById('button-sound').dataset.muted = (this.muted) ? 'true' : 'false'
   }
 }
